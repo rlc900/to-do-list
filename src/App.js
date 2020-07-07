0148496bf887e5e3012bab8976bb04032c2ea6ec
rlc900 <rianna.cleary@gmail.com>
2020-07-07 12:57:16 -0400

    delete function works

--- src/App.js
+++ src/App.js
@@ -32,27 +32,53 @@ function ToDoForm ({ addToDo }) {
   )
 }
 
-const ToDo = ({todo}) => {
+function ToDo(props) {
+
+  const handleDelete = (evt) => {
+    // console.log(props)
+    props.deleteToDo(props.todo.id)
+  }
+
+
   return (
         <List as='ol'>
             <List.Item as='li' value='*' >
-              <List.Content content={todo.text}>
+              <List.Content content={props.todo.text}>
               </List.Content>
             </List.Item>
+            <Button onClick={handleDelete}>Delete</Button>
         </List>
   );
 }
 
+// function ToDoContainer(props) {
+//   // console.log(props)
+//   let toDoArray = props.state.map(toDoObj => (
+//
+//   ))
+//   return (
+//     <div>
+//     </div>
+//   )
+// }
+
+
 function App() {
 
-  const [toDoArr, setToDoArr] = useState([{text: 'yo', id: 0}]);
+  const [toDoArr, setToDoArr] = useState([{text: '', id: 0}]);
 
   // ADDING A TODO
   const addToDo = (text) => {
-    const newId = toDoArr.length + 1
-    const newToDo = {...toDoArr, text: text, id: newId};
-    const modifiedToDoArr = [newToDo, ...toDoArr]
+    let newId = toDoArr.length + 1
+    let newToDo = {...toDoArr, text: text, id: newId};
+    let modifiedToDoArr = [newToDo, ...toDoArr]
     setToDoArr(modifiedToDoArr)
+  }
+
+  // DELETING A TODO
+  const deleteToDo = (id) => {
+    let filteredArr = toDoArr.filter(toDo => toDo.id !== id)
+    setToDoArr(filteredArr)
   }
 
   useEffect(() => {
@@ -73,10 +99,12 @@ function App() {
       <Header as='h1' className='header' textAlign='center'>To Do app!</Header>
       <ToDoForm addToDo={addToDo}/>
       {toDoArr.map((todo, index) => (
-        <ToDo todo={todo} index={index}/>
+        <ToDo todo={todo} index={index} deleteToDo={deleteToDo} state={toDoArr}/>
       ))}
+
     </div>
   );
 }
 
 export default App;
+  // <ToDoContainer deleteTodo={deleteToDo} state={toDoArr}/>
